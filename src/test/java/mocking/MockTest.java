e8491da74c97f91ee3611e6191469e6186d4ea66
Chris Whitty <cwhitty@pivotal.io>
2017-10-11 10:22:41 +0100

    Add test cases

--- src/test/java/mocking/MockTest.java
+++ src/test/java/mocking/MockTest.java
@@ -9,5 +9,37 @@ import static mocking.Mock.verify;
 public class MockTest {
 
 
-   
+    @Test
+    public void shouldNotThrowExceptionWhenExpectationMet() {
+        Collaborator collaborator = mock(Collaborator.class);
+
+        collaborator.collaboratorMethod("some-args");
+
+        verify(collaborator).collaboratorMethod("some-args");
+    }
+
+    @Test(expected = MockVerificationException.class)
+    public void shouldThrowExceptionWhenNotCalled() {
+        Collaborator collaborator = mock(Collaborator.class);
+
+        verify(collaborator).collaboratorMethod("some-args");
+    }
+
+    @Test(expected = MockVerificationException.class)
+    public void shouldThrowExceptionWhenArgsDiffer() {
+        Collaborator collaborator = mock(Collaborator.class);
+
+        collaborator.collaboratorMethod("some-args");
+
+        verify(collaborator).collaboratorMethod("other-args");
+    }
+
+    @Test(expected = MockVerificationException.class)
+    public void shouldThrowExceptionWhenOtherMethodCalled() {
+        Collaborator collaborator = mock(Collaborator.class);
+
+        collaborator.otherMethod("some-args");
+
+        verify(collaborator).collaboratorMethod("other-args");
+    }
 }
