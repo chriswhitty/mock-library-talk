7bfd6334a977c4df6e6f5956dccbb0dbbe9e4e08
Chris Whitty <cwhitty@pivotal.io>
2017-10-11 09:55:03 +0100

    Store invocations

--- src/main/java/mocking/Mock.java
+++ src/main/java/mocking/Mock.java
@@ -8,6 +8,17 @@ import java.util.Map;
 
 public class Mock {
 
+    static class Call {
+        Method method;
+        Object[] args;
+
+        Call(Method method, Object[] args) {
+            this.method = method;
+            this.args = args;
+        }
+    }
+
+    private static Map<Object, Call> mockInteractions = new HashMap<>();
 
     @SuppressWarnings("unchecked")
     public static <T> T mock(Class<T> clazz) {
@@ -15,12 +26,17 @@ public class Mock {
         Class<?>[] classes = new Class<?>[] {clazz};
 
         return (T) Proxy.newProxyInstance(clazz.getClassLoader(), classes, (proxy, method, args) -> {
-            System.out.println("In Proxy");
+            if(method.getName().equals("hashCode")) {
+                return System.identityHashCode(proxy);
+            }
+
+            mockInteractions.put(proxy, new Call(method, args));
             return null;
         });
     }
 
     public static <T> T verify(T mock) {
+        System.out.println(mockInteractions);
         return mock;
     }
 }
